45b53eb373e28a26ed945c10609a6fa6ececcaf9
Yash Seella <71662065+yashhhseella@users.noreply.github.com>
2026-04-20 19:52:46 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def charValue(self, c):
+        return (ord(c) - ord('a'))
+        
+    def isAnagram(self, s: str, t: str) -> bool:
+        countS, countT = {}, {}
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+
+        
